a4164e30adec13c94db4f7b360a23ec2a44f9995
Ian <ian.forrester46@gmail.com>
2022-12-05 09:28:46 -0600

    help

--- app/src/main/java/com/ianmcshoe/taskmaster/MainActivity.java
+++ app/src/main/java/com/ianmcshoe/taskmaster/MainActivity.java
@@ -14,21 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        1. Get UI element by ID
+
         Button addButton = MainActivity.this.findViewById(R.id.MainActivityAddButton);
         Button allButton = MainActivity.this.findViewById(R.id.MainActivityAllButton);
 
-//        2. Add event listener
+
         addButton.setOnClickListener(view -> {
-//        3. CallBack Fn()
-//        4. do stuff in the callback
+
             Intent goToAddTasksActivity = new Intent(MainActivity.this, AddTask.class);
             startActivity(goToAddTasksActivity);
         });
 
         allButton.setOnClickListener(view -> {
-//        3. CallBack Fn()
-//        4. do stuff in the callback
             Intent goToAllTasksActivity = new Intent(MainActivity.this, AllTasks.class);
             startActivity(goToAllTasksActivity);
         });
