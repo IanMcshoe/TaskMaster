4c8d5749dccb21c923db272c6286d2620ee4c3bf
Ian <ian.forrester46@gmail.com>
2022-12-20 01:35:50 -0600

    Location implementation

---
+++ app/src/main/java/com/ianmcshoe/taskmaster/activities/LocationActivity.java
@@ -0,0 +1,17 @@
+package com.ianmcshoe.taskmaster.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ianmcshoe.taskmaster.R;
+
+public class LocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+    }
+
+}
